6fce29d67efa7a3645f211c44264beac87b7e78f
June Kim <godlyjune@gmail.com>
2024-05-05 15:23:53 +0900

    Add styles for current tab

--- components/mainFrame/mainFrameViewTab.tsx
+++ components/mainFrame/mainFrameViewTab.tsx
@@ -1,14 +1,21 @@
+import { useRecoilValue } from 'recoil';
+import { StateCurrentTab } from '../../controllers/data/states';
 import { TypeCompanyTab } from '../../controllers/data/types';
 import styles from './mainFrame.module.scss';
 import { useMoveToTabClick, useRemoveTabClick } from './mainFrameInteractor';
 
 const View = (props: { tab: TypeCompanyTab }) => {
   const { tab } = props;
+  const currentTab = useRecoilValue(StateCurrentTab);
   const onMoveToTabClick = useMoveToTabClick(tab);
   const onRemoveTabClick = useRemoveTabClick(tab);
 
   return (
-    <li title={tab.company.itmsNm} className={styles.tabItem} onClick={onMoveToTabClick}>
+    <li
+      title={tab.company.itmsNm}
+      className={tab.uuid === currentTab.uuid ? styles.tabItemCurrent : styles.tabItem}
+      onClick={onMoveToTabClick}
+    >
       <i
         title="close"
         className={`fa-solid fa-xmark ${styles.tabItemClose}`}
