859d5754d9b3b2d70f23fbe564fc39a3a7d7d765
June Kim <godlyjune@gmail.com>
2024-07-03 18:31:00 +0900

    Split `getDatetimeString()` to re-use it

--- components/mainFrame/mainFrameViewLatestPrice.tsx
+++ components/mainFrame/mainFrameViewLatestPrice.tsx
@@ -1,16 +1,7 @@
 import { TypePriceRequest } from '../../controllers/data/types';
+import { getDatetimeString } from '../../controllers/datetime';
 import { useGetPricesLatest } from '../../controllers/net/price';
 import styles from './mainFrame.module.scss';
-
-const getDatetimeString = (date: Date) => {
-  const year = date.getFullYear();
-  const month = (date.getMonth() + 1).toString().padStart(2, '0');
-  const day = date.getDate().toString().padStart(2, '0');
-  const hour = date.getHours().toString().padStart(2, '0');
-  const minute = date.getMinutes().toString().padStart(2, '0');
-  const second = date.getSeconds().toString().padStart(2, '0');
-  return `at ${year}-${month}-${day} ${hour}:${minute}:${second}`;
-};
 
 const View = (props: { code: string }) => {
   const { code } = props;
