e526c0ab94b1a5ce100c0b5f9a8f0d877ffc50ce
June Kim <godlyjune@gmail.com>
2024-10-22 18:47:03 +0900

    Use `to2DigitString()`
    - Update args of `getDatetimeString()` and `getDatetimeStringForFilename()`
    - Add `getTimestamp()`
    - Refactoring

--- controllers/datetime.ts
+++ controllers/datetime.ts
@@ -1,21 +1,36 @@
-export const getDatetimeString = (date?: Date) => {
+import { TypeIDWeek } from './data/types';
+import { to2DigitString } from './number';
+
+export const getDatetimeString = (date?: Date | TypeIDWeek) => {
   if (!date) return '';
-  const year = date.getFullYear();
-  const month = (date.getMonth() + 1).toString().padStart(2, '0');
-  const day = date.getDate().toString().padStart(2, '0');
-  const hour = date.getHours().toString().padStart(2, '0');
-  const minute = date.getMinutes().toString().padStart(2, '0');
-  const second = date.getSeconds().toString().padStart(2, '0');
+  const { year, month, day, hour, minute, second } = stringfyDatetime(date);
   return `at ${year}-${month}-${day} ${hour}:${minute}:${second}`;
 };
 
-export const getDatetimeStringForFilename = (date?: Date) => {
+export const getDatetimeStringForFilename = (date?: Date | TypeIDWeek) => {
   if (!date) return '';
-  const year = date.getFullYear();
-  const month = (date.getMonth() + 1).toString().padStart(2, '0');
-  const day = date.getDate().toString().padStart(2, '0');
-  const hour = date.getHours().toString().padStart(2, '0');
-  const minute = date.getMinutes().toString().padStart(2, '0');
-  const second = date.getSeconds().toString().padStart(2, '0');
+  const { year, month, day, hour, minute, second } = stringfyDatetime(date);
   return `${year}${month}${day}_${hour}${minute}${second}`;
+};
+
+export const getTimestamp = (date: Date | TypeIDWeek) => {
+  if (date instanceof Date) return date.getTime();
+  return toDate(date).getTime();
+};
+
+const stringfyDatetime = (date: Date | TypeIDWeek) => {
+  if (!(date instanceof Date)) date = toDate(date);
+  const year = date.getFullYear().toString();
+  const month = to2DigitString(date.getMonth() + 1);
+  const day = to2DigitString(date.getDate());
+  const hour = to2DigitString(date.getHours());
+  const minute = to2DigitString(date.getMinutes());
+  const second = to2DigitString(date.getSeconds());
+  return { year, month, day, hour, minute, second };
+};
+
+const toDate = (date: TypeIDWeek) => {
+  const d = new Date(date.year, 0, 1);
+  d.setDate(d.getDate() + 7 * date.week);
+  return d;
 };
